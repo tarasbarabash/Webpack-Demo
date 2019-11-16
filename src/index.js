49500f32e551d39c52c24c0ccfa96047bf60aecd
Taras Barabash <tarasbarabash@gmail.com>
2019-11-16 16:25:02 +0200

    CSS/SCSS support; JS minifier; PWA support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./js/index";
+import "./styles/scss/main.scss";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(registration => console.log("SW registered: ", registration))
+      .catch(registrationError =>
+        console.log("SW registration failed: ", registrationError)
+      );
+  });
+}
