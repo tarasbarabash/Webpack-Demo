fe388ff4c47b6a2193c351953e0d81a56ea68945
Taras Barabash <deploy@travis-ci.org>
2019-12-12 09:01:44 +0000

    Deploy tarasbarabash/Webpack-Template to github.com/tarasbarabash/Webpack-Template.git:gh-pages

---
+++ precache-manifest.3d83b255a45684a06b89720e08cf4fff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "assets/fonts/fonts_go_here"
+  },
+  {
+    "revision": "b467f9732b7b3253d2d4761708ecdb5f",
+    "url": "assets/icons/favicon.ico"
+  },
+  {
+    "revision": "e6984f785e956eedb6ce9a2bc1624e5d",
+    "url": "assets/icons/icon.png"
+  },
+  {
+    "url": "assets/icons/icon_152x152.257f2237ca926b529202cb5e90f0634c.png"
+  },
+  {
+    "url": "assets/icons/icon_384x384.35e48f6dbc724a71252596e8b6d37578.png"
+  },
+  {
+    "url": "assets/icons/icon_512x512.42ab57244f5fe1fe8ea5a9eb4a8475eb.png"
+  },
+  {
+    "url": "assets/icons/icon_72x72.340e09b3307a2837a7b84eec6f5dd838.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "assets/img/images_go_here"
+  },
+  {
+    "url": "css/app.f5c1dc962f3b729ca622.css"
+  },
+  {
+    "revision": "c1ca30f6b87052346342f8ee1f394913",
+    "url": "index.html"
+  },
+  {
+    "url": "js/app.f5c1dc962f3b729ca622.js"
+  },
+  {
+    "url": "js/vendor.f5c1dc962f3b729ca622.js"
+  },
+  {
+    "url": "manifest.2dca598b702cc0627a23bea784bbe2e5.json"
+  }
+]);
